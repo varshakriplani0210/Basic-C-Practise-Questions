afe586191057521c3faede034fc77c9735a2eaf0
varsha kriplani <72504188+varshakriplani0210@users.noreply.github.com>
2021-06-18 19:39:37 +0530

    Program to display alphabets in range

---
+++ Display_Alphabets.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   char ch,c;
+   cout<<"enter a character ";
+   cin>>ch;
+   //for loop
+  for(c='a';c<=ch;c++)
+  {
+      cout<<c;
+  }
+}
