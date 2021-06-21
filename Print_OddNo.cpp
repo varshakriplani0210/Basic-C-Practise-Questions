20029e5651988abc3fdd6091e5c5179eb075f72c
varsha kriplani <72504188+varshakriplani0210@users.noreply.github.com>
2021-06-21 17:11:05 +0530

    Print Odd Numbers using For loop

---
+++ Print_OddNo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            cout<<i<<endl;
+        }
+    }
+}
